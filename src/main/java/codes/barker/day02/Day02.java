fce02b841d7c22cc5bdea1e2ef3460cd2e67f15e
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2025-05-02 10:14:39 +0100

    Solve Day 2 Part 1

---
+++ src/main/java/codes/barker/day02/Day02.java
@@ -0,0 +1,80 @@
+package codes.barker.day02;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+
+public class Day02 {
+    public static void main(String[] args) {
+        int part01 = solvePart01();
+        System.out.println(part01);
+    }
+
+    public static int solvePart01() {
+        List<String> input = readInput();
+        int validPasswords = 0;
+
+        for (String line : input) {
+            HashMap<String, Object> parsed = parseLine(line);
+            String password = (String) parsed.get("password");
+            String letter = (String) parsed.get("letter");
+            int min = (int) parsed.get("min");
+            int max = (int) parsed.get("max");
+
+            if (isValidPassword(password, letter, min, max)) {
+                validPasswords++;
+            }
+        }
+
+        return validPasswords;
+    }
+
+    public static boolean isValidPassword(String password, String target, int min, int max) {
+        String[] letters = password.split("");
+        int count = 0;
+
+        for (String letter : letters) {
+            if (letter.equals(target)) {
+                count++;
+            }
+        }
+
+        return count >= min && count <= max;
+    }
+
+    public static HashMap<String, Object> parseLine(String line) {
+        var parsed = new HashMap<String, Object>();
+
+        String[] temp = line.split(": ");
+        String passwordPolicy = temp[0];
+        String password = temp[1];
+        parsed.put("password", password);
+
+        temp = passwordPolicy.split(" ");
+        String range = temp[0];
+        String letter = temp[1];
+        parsed.put("letter", letter);
+
+        temp = range.split("-");
+        parsed.put("min", Integer.parseInt(temp[0]));
+        parsed.put("max", Integer.parseInt(temp[1]));
+
+        return parsed;
+    }
+
+    public static List<String> readInput() {
+        try (
+            InputStream inputStream = Day02.class.getClassLoader().getResourceAsStream("day02.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        ) {
+            return bufferedReader.lines().toList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return List.of();
+        }
+    }
+}
