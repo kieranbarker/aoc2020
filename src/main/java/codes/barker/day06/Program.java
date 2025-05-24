e2b75823c9f0c63e97ece0c035d140994433e0d5
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2025-05-24 13:05:11 +0100

    Solve Day 6 Part 2

--- src/main/java/codes/barker/day06/Program.java
+++ src/main/java/codes/barker/day06/Program.java
@@ -14,6 +14,9 @@ public class Program {
     public static void main(String[] args) {
         int answer = solve();
         System.out.println(answer);
+
+        int answer2 = solve2();
+        System.out.println(answer2);
     }
 
     private static int solve() {
@@ -27,6 +30,30 @@ public class Program {
             List<String> answerList = Arrays.asList(answers);
             Set<String> answerSet = new HashSet<>(answerList);
             sum += answerSet.size();
+        }
+
+        return sum;
+    }
+
+    private static int solve2() {
+        String input = readInput("day06.txt");
+        String[] groups = input.split("\\r\\n\\r\\n");
+
+        int sum = 0;
+
+        for (String group : groups) {
+            String[] people = group.split("\\r\\n");
+            Set<String> set = new HashSet<>(Arrays.asList(people[0].split("")));
+
+            for (int i = 1; i < people.length; i++) {
+                String[] answers = people[i].split("");
+                List<String> answerList = Arrays.asList(answers);
+                Set<String> answerSet = new HashSet<>(answerList);
+                set.retainAll(answerSet);
+
+            }
+
+            sum += set.size();
         }
 
         return sum;
